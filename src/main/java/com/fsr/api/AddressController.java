28ef9d807b555fd385f4a230099bd6ac48d6d5dd
malinvaudpaul <paulmalinvaud@proton.me>
2022-10-12 17:57:40 +0200

    add CORS policy (for React app)

--- src/main/java/com/fsr/api/AddressController.java
+++ src/main/java/com/fsr/api/AddressController.java
@@ -5,6 +5,7 @@ import com.fsr.services.ServiceAddress;
 import java.net.URI;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 @RequestMapping(path = "/addresses")
 public class AddressController {
 
